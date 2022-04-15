4537ab24a155e8900e1703c58e8b04022235c3ae
Mate Valko <mvalko@apple.com>
2022-04-15 20:41:11 +0200

    a:

--- otp_xls_to_wallet/otp_xls_to_wallet_csv.py
+++ otp_xls_to_wallet/otp_xls_to_wallet_csv.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import os
 from datetime import datetime
-folder_path = (os.path.normpath(os.path.dirname(os.path.realpath(__file__)))+"\\")
-xls_path = folder_path+"xls\\"
-output_path = folder_path+"csv\\"
-"csv\\"
+folder_path = (os.path.normpath(os.path.dirname(os.path.realpath(__file__)))+"/")
+xls_path = folder_path+"xls/"
+output_path = folder_path+"csv/"
 file_list = [f for f in os.listdir(xls_path) if ".xls" in f]
 print (file_list)
 
@@ -12,7 +11,6 @@ actual_table = ""
 for file_name in file_list:
     df = pd.read_html(xls_path+file_name)
     for i,table in enumerate(df):
-        now_date = datetime.now().strftime("%Y%m%d_%H%M%S")
         try:
             if table.columns.values[1] == "Forgalom típusa":
                 print("Meg van a tábla!")
@@ -21,11 +19,11 @@ for file_name in file_list:
                 table.columns = table.columns.str.replace('Összeg', 'Amount')
                 table.columns = table.columns.str.replace('Forgalom típusa', 'Category')
                 table.columns = table.columns.str.replace('Ellenoldali név', 'Payee')
-                table["Note"] =  table["Közlemény"].map(str) + " Bank ID: " + table["Banki tranzakció azonosító"].map(str) + " Ellenoldali számlaszám: " + table["Ellenoldali számlaszám"].map(str)
+                table["Note"] =  table["Közlemény"].map(str) + " Bank ID: " + table["Banki tranzakció azonosító"].map(str) + " Ellenoldali számlaszám: " + table["Ellenoldali számlaszám"].map(str)+" Típus: "+table['Category'].map(str)
                 table.drop(["Közlemény", "Banki tranzakció azonosító","Ellenoldali számlaszám"], axis=1, inplace=True)
                 table["Date"] = pd.to_datetime(table["Date"],format='%Y.%m.%d. %H:%M:%S')
                 table["Date"] = table["Date"].dt.strftime("%d/%m/%Y %H:%M:%S")
-                csv_name = file_name.split('.')[0]+"_"+now_date+".csv"
+                csv_name = "Mate_"+file_name.split('.')[0]+".csv"
                 print("{0} fájl mentése...".format(csv_name))
                 table.to_csv(output_path+csv_name,sep=';',encoding='utf-8-sig',index=False, na_rep=None)
         except Exception as e:
